b4c5591de7b612604e24364f39c3311b746eb745
Francisco Rafael Checa Molina <manzanile@gmail.com>
2017-04-23 00:13:32 +0200

    Añadido nuevo fondo. En desarrollo collider de límite de escenario.

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -14,26 +14,32 @@ public class move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
+
+		if (Input.GetKey (KeyCode.LeftArrow)) { // LEFT
 			transform.position += Vector3.left * speed * Time.deltaTime;
 			animator.StopPlayback ();
-			UpdateState("move_character_left");
-		}
-		else
-			if (Input.GetKey(KeyCode.RightArrow))
-			{
-				transform.position += Vector3.right * speed * Time.deltaTime;
-				animator.StopPlayback ();
-				UpdateState("move_character_right");
-			}
-			else
-				UpdateState("idle_character");
+			UpdateState ("move_character_left");
+		} else if (Input.GetKey (KeyCode.RightArrow)) { // RIGHT
+			transform.position += Vector3.right * speed * Time.deltaTime;
+			animator.StopPlayback ();
+			UpdateState ("move_character_right");
+		} else if (Input.GetKey (KeyCode.Z)) { // JUMP (Z)
+			//transform.position.y
+		} else
+			UpdateState("idle_character");
 	}
 
 	public void UpdateState(string state = null) {
 		if (state != null) {
 			animator.Play(state); // Ejecutar la animación pasada como parámetro
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Colisión con el límite del escenario");
+		if (other.gameObject.tag == "brick") {
+			Debug.Log("Colisión con el límite del escenario");
+			speed = 0;
 		}
 	}
 		
