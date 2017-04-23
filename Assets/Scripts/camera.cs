6f0bb3088077ca7503113b93d511ca259dff0ce8
Francisco Rafael Checa Molina <manzanile@gmail.com>
2017-04-24 00:03:04 +0200

    Cambios en la implementación de los colliders de limitación de escenario. Ahora se detiene all llegar al FINAL del escenario.

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class camera : MonoBehaviour {
 
 	public GameObject player;
+	bool fin = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,8 +14,14 @@ public class camera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () { 
-		if (player.transform.position.x > 0.2f) {
+		if (player.transform.position.x > 0.2f && !fin) {
+			Debug.Log ("Hola");
 			this.gameObject.transform.position = new Vector3 (player.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+		}
+
+		if (player.transform.position.x > 77f) {
+			fin = true;
+			this.gameObject.transform.position = new Vector3 (77f, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
 		}
 	}
 }
--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direccion {Izquierda, Derecha};
+
 public class move : MonoBehaviour {
 
 	public GameObject bulletPrefab;
 
 	public float speed = 2.5f;
 	private Animator animator;
-	bool collision = false;
 	public float jumpPower = 2.5f;
 	private bool jump;
 	bool grounded = false;
+	bool collision_init = false;
+	bool collision_fin = false;
+
+	public Direccion direccion = Direccion.Derecha;
 
 	private Rigidbody2D rb2d;
 
@@ -28,15 +33,20 @@ public class move : MonoBehaviour {
 		animator.SetBool("Grounded",grounded);
 
 		if (Input.GetKey (KeyCode.LeftArrow)) { // LEFT
+			if (collision_fin) {
+				speed = 2.5f;
+				collision_init = false;
+			}
+			direccion = Direccion.Izquierda;
 			transform.position += Vector3.left * speed * Time.deltaTime;
-			//animator.StopPlayback ();
 			UpdateState ("move_character_left_gun");
 		} else if (Input.GetKey (KeyCode.RightArrow)) { // RIGHT
-			if (collision) { // Si estamos en el borde del escenario, para moverse
+			if (collision_init) {
 				speed = 2.5f;
+				collision_init = false;
 			}
+			direccion = Direccion.Derecha;
 			transform.position += Vector3.right * speed * Time.deltaTime;
-			//animator.StopPlayback ();
 			UpdateState ("move_character_right_gun");
 		} else
 			UpdateState("idle_gun_character");
@@ -51,6 +61,7 @@ public class move : MonoBehaviour {
 			position.x += 0.65f;
 			Instantiate(bulletPrefab,position,Quaternion.identity);
 		}
+
 	}
 
 	void FixedUpdate() {
@@ -69,7 +80,12 @@ public class move : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		Debug.Log ("Colisión con el límite del escenario");
 		if (other.gameObject.tag == "brick") {
-			collision = true;
+			collision_init = true;
+			speed = 0;
+		}
+		// End of street
+		if (other.gameObject.tag == "brick2") {
+			collision_fin = true;
 			speed = 0;
 		}
 	}
