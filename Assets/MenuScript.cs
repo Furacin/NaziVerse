c652f56687d941930c7735eedeba08445404977f
Francisco Rafael Checa Molina <manzanile@gmail.com>
2017-04-23 19:25:43 +0200

    Añadido menú inicial.

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+
+	public void changemenuscene(string scene) {
+		Application.LoadLevel (scene);
+	}
+
+	public void exitgame() {
+		Application.Quit();
+	}
+}
---
+++ Assets/NaziGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaziGenerator : MonoBehaviour {
+
+	public GameObject naziPrefab;
+	public float generatorTime = 1.75f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateNazi() {
+		Instantiate(naziPrefab,transform.position, Quaternion.identity);
+	}
+
+	public void StartGenerator() {
+		InvokeRepeating ("CreateNazi",0f,generatorTime);
+	}
+}
