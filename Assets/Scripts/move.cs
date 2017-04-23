a2a6135e56f3d1063b1298ec533acab479687895
Francisco Rafael Checa Molina <manzanile@gmail.com>
2017-04-23 14:12:08 +0200

    Añadido salto con fuerzas (gravedad) sobre el personaje.

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -7,10 +7,15 @@ public class move : MonoBehaviour {
 	public float speed = 2.5f;
 	private Animator animator;
 	bool collision = false;
+	public float jumpPower = 2.5f;
+	private bool jump;
+
+	private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> (); // Obtenemos la componente animator del objeto
+		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -18,19 +23,29 @@ public class move : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.LeftArrow)) { // LEFT
 			transform.position += Vector3.left * speed * Time.deltaTime;
-			animator.StopPlayback ();
+			//animator.StopPlayback ();
 			UpdateState ("move_character_left_gun");
 		} else if (Input.GetKey (KeyCode.RightArrow)) { // RIGHT
 			if (collision) { // Si estamos en el borde del escenario, para moverse
 				speed = 2.5f;
 			}
 			transform.position += Vector3.right * speed * Time.deltaTime;
-			animator.StopPlayback ();
+			//animator.StopPlayback ();
 			UpdateState ("move_character_right_gun");
-		} else if (Input.GetKey (KeyCode.Z)) { // JUMP (Z)
-			//transform.position.y
 		} else
 			UpdateState("idle_gun_character");
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			jump = true;
+
+		}
+	}
+
+	void FixedUpdate() {
+		if (jump) {
+			rb2d.AddForce (Vector2.up * jumpPower, ForceMode2D.Impulse);
+			jump = false;
+		}
 	}
 
 	public void UpdateState(string state = null) {
