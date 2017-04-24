0ad806e9ec2e3ee5d42a88c6ad4211d1ab8b4b48
Francisco Rafael Checa Molina <manzanile@gmail.com>
2017-04-24 02:20:09 +0200

    Completado un poco más el level 2. Se generan nazis veganos y se pueden matar con la pistola.

---
+++ Assets/Scripts/GreenNaziGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenNaziGenerator : MonoBehaviour {
+
+	public GameObject greenNaziPrefab;
+	public float generatorTime = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void CreateNazi() {
+		Instantiate(greenNaziPrefab,transform.position, Quaternion.identity);
+	}
+
+	public void StartGenerator() {
+		InvokeRepeating ("CreateNazi",0f,generatorTime);
+	}
+}
